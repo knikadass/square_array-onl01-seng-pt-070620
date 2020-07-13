befc8fb1d2aa32eccd46c9671066ce34ee4a3ca1
Knika Dass <knikadass@gmail.com>
2020-07-13 03:53:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,9 @@
 def square_array(array)
-  square_array = ([1, 2, 3])
+  square_array = [1, 2, 3]
   array.each do |number|
     square_array << number ** 2
   puts #{number} 
 end
 
-return square_array
 
 end
